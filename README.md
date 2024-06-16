de39d938f1426debb7137f55ea3ab36e6b0db9d1
HenriqueMendesCoelho <henriquemendescoelho@gmail.com>
2024-06-16 09:50:36 -0300

    docs: Update README.md with detailed project information and configuration

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# KronusGateway
+# Project Cine Kronusboss - API Gateway
+
+## Overview
+
+This project is an API Gateway for Cine KronusBoss, built using Kotlin and Spring Cloud Gateway. The gateway serves as a
+single entry point for all client requests, routing them to the appropriate backend services. It provides features such
+as routing, request filtering, and global CORS configuration.
+
+## Features
+
+- **Global CORS Configuration**: Allows cross-origin requests from any origin.
+- **Routing**: Routes client requests to various backend services based on URL paths.
+- **Request Filtering**: Implements custom filters for authentication and authorization.
+
+## Dependencies
+
+- Kotlin
+- Spring Cloud Gateway
---
+++ build.gradle.kts
@@ -0,0 +1,49 @@
+plugins {
+    id("org.springframework.boot") version "3.3.0"
+    id("io.spring.dependency-management") version "1.1.5"
+    kotlin("jvm") version "1.9.24"
+    kotlin("plugin.spring") version "1.9.24"
+}
+
+group = "com.kronusboss.cine"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+extra["springCloudVersion"] = "2023.0.2"
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.springframework.cloud:spring-cloud-starter-gateway")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("io.jsonwebtoken:jjwt-api:0.12.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.5")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+    }
+}
+
+kotlin {
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict")
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
