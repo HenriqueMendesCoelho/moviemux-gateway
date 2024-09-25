7d11901eb18d7c4eae224e1898c50bf4635bd0d2
HenriqueMendesCoelho <henriquemendescoelho@gmail.com>
2024-09-24 22:32:59 -0300

    feat: Add missing JWTUtil class
    - Added the JWTUtil class that was missing in the previous commit
    - Resolves build errors related to unresolved reference to JWTUtil in KeyResolverConfiguration and JwtAuthenticationFilter

---
+++ src/main/kotlin/com/kronusboss/cine/gateway/util/JWTUtil.kt
@@ -0,0 +1,30 @@
+package com.kronusboss.cine.gateway.util
+
+import io.jsonwebtoken.Claims
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.security.Keys
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+import java.nio.charset.StandardCharsets
+import javax.crypto.SecretKey
+
+@Component
+class JWTUtil {
+
+    @Value("\${jwt.secret}")
+    lateinit var secret: String
+    fun getClaims(token: String): Claims? {
+        return try {
+            Jwts.parser().verifyWith(getSigningKey()).build().parseSignedClaims(token).payload
+        } catch (e: java.lang.Exception) {
+            println("Error parsing token: ${e.message}")
+            null
+        }
+    }
+
+    private fun getSigningKey(): SecretKey {
+        val keyBytes = secret.toByteArray(StandardCharsets.UTF_8)
+        return Keys.hmacShaKeyFor(keyBytes)
+    }
+
+}
